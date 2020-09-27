1c72058c115a8472b077115881ee0314f238f85d
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-27 15:35:49 +0800

    Add files via upload

---
+++ XOR Operation in an Array.py
@@ -0,0 +1,13 @@
+#XOR Operation in an Array
+
+class Solution(object):
+    def xorOperation(self, n, start):
+        """
+        :type n: int
+        :type start: int
+        :rtype: int
+        """
+        res = 0
+        for i in range(n):
+            res ^= (start + 2*i)
+        return res
